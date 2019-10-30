89e4136795d613ab00683fb7bca81848c2f71649
leonCaptain <leon522886@gmail.com>
2019-10-30 08:22:10 +0800

    out put static file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 module.exports = {
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/' },
+      '/index': { page: '/index' },
+      '/hosting': { page: '/hosting' },
+      '/unfind': { page: '/unfind' }
+    }
+  },
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.jpg$/,
       use: ["file-loader"],
     })
-
     return config
   },
 }
